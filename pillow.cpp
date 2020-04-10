93bc4141353040d91026bc7ae734663a55945b24
Joseph Sigbeku <josephsgbku@protonmail.com>
2020-04-10 13:57:46 +0100

    z-buffering fixed

--- pillow.cpp
+++ pillow.cpp
@@ -18,10 +18,10 @@
 //Create frame buffer
 
 struct vertex {
-	double x;
-	double y;
-	double z;
-	double w;
+	float x;
+	float y;
+	float z;
+	float w;
 };
 
 struct Color {
@@ -31,33 +31,33 @@ struct Color {
 };
 
 struct edge_pixel {	
-	double x;
+	float x;
 	struct Color c;
-	double depth;
+	float depth;
 };
 
 struct viewport {
-	double x;
-	double y;
-	double w;
-	double h;
+	float x;
+	float y;
+	float w;
+	float h;
 };
 
 struct vector3D {
-	double x;
-	double y;
-	double z;
+	float x;
+	float y;
+	float z;
 };
 
 bool no_clipping=false;
 long selected = 0;
 const struct viewport vp = {150,50,800,600};
-const double far = 12;
-const double near = 2;
+const float far = 30;
+const float near = 2;
 const int fov = 110;
-const double S = 1/(tan((fov/2)*(M_PI/180)));
-const double aspect_ratio = (double) WIN_WIDTH/WIN_HEIGHT;
-const double pm[4][4] = {
+const float S = 1/(tan((fov/2)*(M_PI/180)));
+const float aspect_ratio = (float) WIN_WIDTH/WIN_HEIGHT;
+const float pm[4][4] = {
 	
 	{ S/aspect_ratio, 0, 0, 0 },
 	{ 0, S, 0, 0 },
@@ -69,7 +69,7 @@ const double pm[4][4] = {
 class Camera {
 	
 	public:
-		double transform[4][4] = {
+		float transform[4][4] = {
 			
 			{ 1, 0, 0, 0 },
 			{ 0, 1, 0, 0 },
@@ -87,11 +87,11 @@ class Camera {
 class Vec3 {
 
 	public:
-		double x;
-		double y;
-		double z;
+		float x;
+		float y;
+		float z;
 		
-		Vec3(double x, double y, double z) {
+		Vec3(float x, float y, float z) {
 			this->x=x;
 			this->y=y;
 			this->z=z;
@@ -119,7 +119,7 @@ class Vec3 {
 	
 		}		
 
-		double dot(Vec3 v) {
+		float dot(Vec3 v) {
 			return (this->x*v.x) + (this->y*v.y) + (this->z*v.z);
 		}
 
@@ -134,7 +134,7 @@ class Mesh {
 		std::vector<std::vector<long>> tf_list;
 		std::vector<struct vector3D> n_list;
 		std::string name;
-		double max = 1;		
+		float max = 1;		
 
 		Mesh(std::string title) {
 			this->name=title;
@@ -183,7 +183,7 @@ class Mesh {
 			}
 		}
 
-		void add_normal(double x, double y, double z) {
+		void add_normal(float x, float y, float z) {
 			struct vector3D n = {x ,y ,z};
 			this->n_list.push_back(n);	
 		}
@@ -193,7 +193,7 @@ class Mesh {
 		}
 
 
-		void add_vertex(double x, double y, double z) {
+		void add_vertex(float x, float y, float z) {
 			struct vertex v = {x ,y ,z, 1};
 			this->v_list.push_back(v);	
 		}
@@ -242,9 +242,9 @@ class Mesh {
 			printf("\n");
 		}
 
-		void scale(double x, double y, double z) {
+		void scale(float x, float y, float z) {
 				
-			double scale_mat[4][4] = {
+			float scale_mat[4][4] = {
 					
 				{ x, 0, 0, 0 },
 				{ 0, y, 0, 0 },
@@ -257,10 +257,10 @@ class Mesh {
 			
 		}
 		
-		void rotate_x(double angle) {
+		void rotate_x(float angle) {
 
-			double t = ((angle*M_PI)/180);
-			double rot_x[4][4] = {
+			float t = ((angle*M_PI)/180);
+			float rot_x[4][4] = {
 					
 				{   1,      0,      0,      0   },
 				{   0,   cos(t), -sin(t),   0   },
@@ -272,10 +272,10 @@ class Mesh {
 
 		}
 
-		void rotate_y(double angle) {
+		void rotate_y(float angle) {
 
-			double t = ((angle*M_PI)/180);
-			double rot_y[4][4] = {
+			float t = ((angle*M_PI)/180);
+			float rot_y[4][4] = {
 					
 				{ cos(t),   0,    sin(t),   0   },
 				{   0,      1,      0,      0   },
@@ -287,10 +287,10 @@ class Mesh {
 
 		}
 
-		void rotate_z(double angle) {
+		void rotate_z(float angle) {
 
-			double t = ((angle*M_PI)/180);
-			double rot_z[4][4] = {
+			float t = ((angle*M_PI)/180);
+			float rot_z[4][4] = {
 					
 				{ cos(t),-sin(t),   0,      0   },
 				{ sin(t), cos(t),   0,      0   },
@@ -302,9 +302,9 @@ class Mesh {
 
 		}
 
-		void translate(double x, double y, double z) {
+		void translate(float x, float y, float z) {
 
-			double trans[4][4] = {
+			float trans[4][4] = {
 
 				{ 1, 0, 0, 0 },
 				{ 0, 1, 0, 0 },
@@ -316,14 +316,14 @@ class Mesh {
 
 		}
 		
-		void apply_transform(double tm[4][4]) {
+		void apply_transform(float tm[4][4]) {
 
 			for	(long i = 0; i<this->vertices(); i++) {
 			
-				double x = ( this->v_list[i].x * tm[0][0] ) + ( this->v_list[i].y * tm[1][0] ) + ( this->v_list[i].z * tm[2][0] ) + ( this->v_list[i].w * tm[3][0] );
-				double y = ( this->v_list[i].x * tm[0][1] ) + ( this->v_list[i].y * tm[1][1] ) + ( this->v_list[i].z * tm[2][1] ) + ( this->v_list[i].w * tm[3][1] );
-				double z = ( this->v_list[i].x * tm[0][2] ) + ( this->v_list[i].y * tm[1][2] ) + ( this->v_list[i].z * tm[2][2] ) + ( this->v_list[i].w * tm[3][2] );
-				double w = ( this->v_list[i].x * tm[0][3] ) + ( this->v_list[i].y * tm[1][3] ) + ( this->v_list[i].z * tm[2][3] ) + ( this->v_list[i].w * tm[3][3] );
+				float x = ( this->v_list[i].x * tm[0][0] ) + ( this->v_list[i].y * tm[1][0] ) + ( this->v_list[i].z * tm[2][0] ) + ( this->v_list[i].w * tm[3][0] );
+				float y = ( this->v_list[i].x * tm[0][1] ) + ( this->v_list[i].y * tm[1][1] ) + ( this->v_list[i].z * tm[2][1] ) + ( this->v_list[i].w * tm[3][1] );
+				float z = ( this->v_list[i].x * tm[0][2] ) + ( this->v_list[i].y * tm[1][2] ) + ( this->v_list[i].z * tm[2][2] ) + ( this->v_list[i].w * tm[3][2] );
+				float w = ( this->v_list[i].x * tm[0][3] ) + ( this->v_list[i].y * tm[1][3] ) + ( this->v_list[i].z * tm[2][3] ) + ( this->v_list[i].w * tm[3][3] );
 				
 				this->v_list[i].x = x;
 				this->v_list[i].y = y;
@@ -397,7 +397,7 @@ void load_model(std::string path) {
 }
 
 unsigned char buffer[WIN_WIDTH][WIN_HEIGHT][4];
-double depth_buffer[WIN_WIDTH][WIN_HEIGHT];
+float depth_buffer[WIN_WIDTH][WIN_HEIGHT];
 	
 void clear_buffer() {
 	for (int i = 0; i<WIN_WIDTH; i++) {
@@ -409,9 +409,9 @@ void clear_buffer() {
 	for (int i = 0; i<WIN_WIDTH; i++) {
 		for (int j = 0; j<WIN_HEIGHT; j++) {
 			buffer[i][j][0]=255;	
-			buffer[i][j][1]=170;
-			buffer[i][j][2]=170;
-			buffer[i][j][3]=170;
+			buffer[i][j][1]=255;
+			buffer[i][j][2]=255;
+			buffer[i][j][3]=255;
 		}
 	}
 }
@@ -433,7 +433,7 @@ void flip_buffer(SDL_Renderer* renderer, SDL_Texture* Frame) {
 	SDL_RenderPresent(renderer);
 }
 
-void set_pixel(int x, int y, unsigned char* color,double depth) {
+void set_pixel(int x, int y, unsigned char* color,float depth) {
 
 	if (x<0 || x>=WIN_WIDTH) {
 		//////printf("clipped x\n");
@@ -444,7 +444,7 @@ void set_pixel(int x, int y, unsigned char* color,double depth) {
 		return;
 	}
 
-	double d = depth_buffer[x][y];
+	float d = depth_buffer[x][y];
 	//printf("comparing %f with %f\n",d,depth);
 	if (depth<d) {
 		depth_buffer[x][y]=depth;
@@ -474,30 +474,30 @@ void get_pairs(std::vector<struct vector3D> poly_r,
 		unsigned char g1 = v_a[k+1].g;
 		unsigned char b1 = v_a[k+1].b;
 
-		double s_x = poly_r[k].x;
-		double s_y = poly_r[k].y;
-		double s_z = poly_r[k].z;
-		double e_x = poly_r[k+1].x; 
-		double e_y = poly_r[k+1].y;
-		double e_z = poly_r[k+1].z;
+		float s_x = poly_r[k].x;
+		float s_y = poly_r[k].y;
+		float s_z = poly_r[k].z;
+		float e_x = poly_r[k+1].x; 
+		float e_y = poly_r[k+1].y;
+		float e_z = poly_r[k+1].z;
 
 		int sx = (int) s_x;
 		int sy = (int) s_y;
 		int ex = (int) e_x; 
 		int ey = (int) e_y;
-		double sz = s_z; 
-		double ez = e_z;
+		float sz = s_z; 
+		float ez = e_z;
 
-		double dy = (double) ey-sy;
-		double dx = (double) ex-sx;
-		double dydx = (double) dy/dx;	
-		double dxdy = 1/dydx;
+		float dy = (float) ey-sy;
+		float dx = (float) ex-sx;
+		float dydx = (float) dy/dx;	
+		float dxdy = 1/dydx;
 	
 		if (dydx>1 || dydx<-1) {
 			
-			double r_inc = (r1-r)/dy;
-			double g_inc = (g1-g)/dy;
-			double b_inc = (b1-b)/dy;
+			float r_inc = (r1-r)/dy;
+			float g_inc = (g1-g)/dy;
+			float b_inc = (b1-b)/dy;
 
 			if (s_y>e_y){
 				sy=e_y;
@@ -506,9 +506,9 @@ void get_pairs(std::vector<struct vector3D> poly_r,
 				sz=e_z;
 				ez=s_z;
 			}
-			double z_inc = (ez-sz)/dy;		
+			float z_inc = (ez-sz)/(ey-sy);		
 		
-			double i = (double) sx;
+			float i = (float) sx;
 			int inc = 0;
 			for (int j = sy; j<ey; j++) {
 				struct Color col = {
@@ -516,31 +516,32 @@ void get_pairs(std::vector<struct vector3D> poly_r,
 					g+(inc*g_inc),
 					b+(inc*b_inc)
 				};
-				double depth = sz+((j-sy)*z_inc);
-				struct edge_pixel n = {i, col,1/depth};
+				float depth = sz+((j-sy)*z_inc);
+				struct edge_pixel n = {i, col,depth};
 				edges[(int)(j-min)].push_back(n);	
-				unsigned char c[4] = {col.r,col.g,col.b,255};
-				set_pixel((int)i,(int)j,c,1/depth);
+				unsigned char c[4] = {43,107,100,255};
+				set_pixel((int)i,(int)j,c,(1/depth));
 				i+=dxdy;
 				inc++;
 			}
 		}
 		else {
 
-			double r_inc = (r1-r)/dx;
-			double g_inc = (g1-g)/dx;
-			double b_inc = (b1-b)/dx;
+			float r_inc = (r1-r)/dx;
+			float g_inc = (g1-g)/dx;
+			float b_inc = (b1-b)/dx;
 			
 			if (s_x>e_x){
 				sx=e_x;
 				ex=s_x;
 				sy=e_y;
+				ey=s_y;
 				sz=e_z;
 				ez=s_z;
 			}
-			double z_inc = (ez-sz)/dy;		
 
-			double j = (double) sy;
+			float z_inc = (ez-sz)/(ey-sy);		
+			float j = (float) sy;
 			int inc = 0;
 			for (int i = sx; i<ex; i++) {
 				struct Color col = {
@@ -548,11 +549,11 @@ void get_pairs(std::vector<struct vector3D> poly_r,
 					g+(inc*g_inc),
 					b+(inc*b_inc)
 				};
-				double depth = sz+((j-sy)*z_inc);
-				struct edge_pixel n = {i,col,1/depth};
+				float depth = sz+((j-sy)*z_inc);
+				struct edge_pixel n = {i,col,depth};
 				edges[(int)(j-min)].push_back(n);	
-				unsigned char c[4] = {col.r,col.g,col.b,255};
-				set_pixel((int)i,(int)j,c,1/depth);
+				unsigned char c[4] = {43,107,100,255};
+				set_pixel((int)i,(int)j,c,(1/depth));
 				j+=dydx;
 				inc++;
 			}
@@ -560,14 +561,14 @@ void get_pairs(std::vector<struct vector3D> poly_r,
 	}
 }
 
-void draw_rect(double x, double y, double w, double h, unsigned char* color) {
+void draw_rect(float x, float y, float w, float h, unsigned char* color) {
 	//draw_line(x,y,x+w,y,color);
 	//draw_line(x+w,y,x+w,y+h,color);
 	//draw_line(x+w,y+h,x,y+h,color);
 	//draw_line(x,y+h,x,y,color);
 }
 
-double dot3D (struct vector3D v1, struct vector3D v2) {
+float dot3D (struct vector3D v1, struct vector3D v2) {
 	return (v1.x*v2.x)+(v1.y*v2.y)+(v1.z*v2.z);
 }
 
@@ -596,18 +597,18 @@ void clip_triangle(std::vector<long>& ply, std::vector<struct vertex> &new_poly,
 
 		//Implementation of Liang Barsky line clipping <- (okay this should remind you, otherwise, you're screwed)
 		if (e->z<=e->w && s->z>=s->w) {
-			double t = (s->w-e->z)/(s->z-e->z);
-			double nx = e->x+(t*(s->x-e->x));
-			double ny = e->y+(t*(s->y-e->y));
+			float t = (s->w-e->z)/(s->z-e->z);
+			float nx = e->x+(t*(s->x-e->x));
+			float ny = e->y+(t*(s->y-e->y));
 			struct vertex n = {nx,ny,s->w,s->w};
 			struct vertex n1 = {e->x,e->y,e->z,e->w};
 			new_poly.push_back(n);
 			new_poly.push_back(n1);
 		}
 		else if (s->z<=s->w && e->z>=e->w) {
-			double t = (e->w-s->z)/(e->z-s->z);
-			double nx = s->x+(t*(e->x-s->x));
-			double ny = s->y+(t*(e->y-s->y));
+			float t = (e->w-s->z)/(e->z-s->z);
+			float nx = s->x+(t*(e->x-s->x));
+			float ny = s->y+(t*(e->y-s->y));
 			struct vertex n = {nx,ny,e->w,e->w};
 			//struct vertex n1 = {s->x,s->y,s->z,s->w};
 			//new_poly.push_back(n1);		
@@ -644,11 +645,11 @@ void render_mesh(Mesh *m, Camera *camera) {
 	unsigned char color[4] = {120,120,120,255};
 	std::vector <struct vertex>clip_coords;
 	////printf("==================\n");
-	double sf = 1.4;
+	float sf = 10;
 	m->scale(sf,sf,sf);
-	double tx = 0.0f;
-	double ty = -0.3f;
-	double tz = -2.0f;
+	float tx = 0.0f;
+	float ty = -3.0f;
+	float tz = -16.0f;
 	m->rotate_y(1.5f);
 	m->translate(tx,ty,tz);
 
@@ -656,10 +657,10 @@ void render_mesh(Mesh *m, Camera *camera) {
 		
 		struct vertex* v = &(m->v_list[vtx]);
 		//Applying matrix projection to enter clip space				
-		double x = (double) ( v->x * pm[0][0] ) + ( v->y * pm[1][0] ) + ( v->z * pm[2][0] ) + ( v->w * pm[3][0] );
-		double y = (double) ( v->x * pm[0][1] ) + ( v->y * pm[1][1] ) + ( v->z * pm[2][1] ) + ( v->w * pm[3][1] );
-		double z = (double) ( v->x * pm[0][2] ) + ( v->y * pm[1][2] ) + ( v->z * pm[2][2] ) + ( v->w * pm[3][2] );
-		double w = (double) ( v->x * pm[0][3] ) + ( v->y * pm[1][3] ) + ( v->z * pm[2][3] ) + ( v->w * pm[3][3] );	
+		float x = (float) ( v->x * pm[0][0] ) + ( v->y * pm[1][0] ) + ( v->z * pm[2][0] ) + ( v->w * pm[3][0] );
+		float y = (float) ( v->x * pm[0][1] ) + ( v->y * pm[1][1] ) + ( v->z * pm[2][1] ) + ( v->w * pm[3][1] );
+		float z = (float) ( v->x * pm[0][2] ) + ( v->y * pm[1][2] ) + ( v->z * pm[2][2] ) + ( v->w * pm[3][2] );
+		float w = (float) ( v->x * pm[0][3] ) + ( v->y * pm[1][3] ) + ( v->z * pm[2][3] ) + ( v->w * pm[3][3] );	
 		struct vertex c = {x,y,z,w};
 		clip_coords.push_back(c);
 
@@ -700,9 +701,9 @@ void render_mesh(Mesh *m, Camera *camera) {
 			
 			struct vertex *v = new_poly[k];
 
-			double x = (v->x/v->w)*WIN_WIDTH + (WIN_WIDTH/2);
-			double y = (-v->y/v->w)*WIN_HEIGHT + (WIN_HEIGHT/2);
-			double z = v->z/v->w;
+			float x = (v->x/v->w)*WIN_WIDTH + (WIN_WIDTH/2);
+			float y = (-v->y/v->w)*WIN_HEIGHT + (WIN_HEIGHT/2);
+			float z = v->z/v->w;
 			
 			struct vector3D r1 = {x,y,1/z};
 			poly_r.push_back(r1);
@@ -721,21 +722,39 @@ void render_mesh(Mesh *m, Camera *camera) {
 		
 		//RASTER SPACE
 		std::vector <struct Color> vertex_attributes = {
-			{200,200,200},
-			{200,200,200},
-			{200,200,200},
-			{200,200,200},
+				
+			{159,229,221},
+			{159,229,221},
+			{159,229,221},
+			{159,229,221},
+
+/*
+			{255,255,255},
+			{255,255,255},
+			{255,255,255},
+			{255,255,255},
+
+			{120,120,120},
+			{120,120,120},
+			{120,120,120},
+			{120,120,120},
+
+			{255,0,0},
+			{0,255,0},
+			{0,0,255},
+			{255,0,0},
+*/
 		};
 	
 		const int range = max-min;	
 		std::vector <std::vector<struct edge_pixel>> pairs(range+1);
 		get_pairs(poly_r,pairs,vertex_attributes,min);
-		for (int l = 0; l<range; l++) {
+		for (int l = 0; l<range+1; l++) {
 			std::vector<struct edge_pixel> s = pairs[l];
 
 			int yval = l+min;
-			double smallest = 100000000;
-			double largest = -100000000;
+			float smallest = 100000000;
+			float largest = -100000000;
 			int is = 0;
 			int il = 0;
 			//printf("=============\n");
@@ -761,40 +780,43 @@ void render_mesh(Mesh *m, Camera *camera) {
 				continue;
 			}
 
-			double first = smallest;
-			double last = largest;
+			float first = smallest;
+			float last = largest;
 				
-			double startz = s[is].depth;			
-			double endz = s[il].depth;			
+			float startz = s[is].depth;			
+			float endz = s[il].depth;			
 				
-			double startr = s[is].c.r;			
-			double startg = s[is].c.g;			
-			double startb = s[is].c.b;			
+			float startr = s[is].c.r;			
+			float startg = s[is].c.g;			
+			float startb = s[is].c.b;			
 
 
-			double r_inc = (s[il].c.r - s[is].c.r)/(last-first);
-			double g_inc = (s[il].c.g - s[is].c.g)/(last-first);
-			double b_inc = (s[il].c.b - s[is].c.b)/(last-first);
-			double z_inc = (endz - startz)/(last-first);
+			float r_inc = (s[il].c.r - s[is].c.r)/(last-first);
+			float g_inc = (s[il].c.g - s[is].c.g)/(last-first);
+			float b_inc = (s[il].c.b - s[is].c.b)/(last-first);
+			float z_inc = (endz - startz)/(last-first);
+		
+			for (int n=first; n<last; n++) {		
+				unsigned char r = startr+(r_inc*(n-first));
+				unsigned char g = startg+(g_inc*(n-first));
+				unsigned char b = startb+(b_inc*(n-first));
+				float z = startz+(z_inc*(n-first));
+				unsigned char color[4] = {
+					r,g,b,255
+				};
+				set_pixel(n,yval,color,1/z);
+			}
 				
-			unsigned char g = startg+(g_inc*(n-first));
-			unsigned char b = startb+(b_inc*(n-first));
-			double z = startz+(z_inc*(n-first));
-			unsigned char color[4] = {
-				r,g,b,255
-			};
-				
-			set_pixel(n,yval,color,z);
 		
 			//struct edge_pixel x2 = pairs[i][pairs[i].size()-1];
 		}
 
 		//for wireframe
-		for (int j = 0; j<poly_r.size()-1; j++) {
-			struct vector3D v = poly_r[j];
-			struct vector3D v1 = poly_r[j+1];
+		//for (int j = 0; j<poly_r.size()-1; j++) {
+		//	struct vector3D v = poly_r[j];
+		//	struct vector3D v1 = poly_r[j+1];
 			//draw_line(v.x,v.y,v1.x,v1.y,color);
-		}
+		//}
 	}
 	
 
@@ -824,7 +846,7 @@ void initialize() {
 	load_model("models/tank.obj");
 	load_model("models/drill.obj");
 
-	selected = 5;
+	selected = 6;
 	models[selected]->normalize();
 	models[selected]->triangulate();
 	models[selected]->print_mesh();
