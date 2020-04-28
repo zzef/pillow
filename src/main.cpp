b31600ab22f54e4f5cc163e574aa28ab7bf51284
Joseph <josephsgbku@protonmail.com>
2020-04-28 17:56:52 +0100

    nm

--- src/main.cpp
+++ src/main.cpp
@@ -336,7 +336,7 @@ void render_mesh(Mesh *m) {
 	float sf = 12;
 	m->scale(sf,sf,sf);
 	float tx = 0.0f;
-	float ty = -4.0f;
+	float ty = -3.0f;
 	float tz = -30.0f;
 	m->rotate_y(1.5f);
 	m->translate(tx,ty,tz);
