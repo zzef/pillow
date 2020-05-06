04bec87702e53fcb1d77317441a10224fbce1579
Joseph <josephsgbku@protonmail.com>
2020-05-06 17:38:45 +0100

    qs

--- src/main.cpp
+++ src/main.cpp
@@ -439,7 +439,7 @@ void render_mesh(Mesh *m, Camera *camera) {
 	//m->rotate_y(1.5f);
 	m->rotate_x(-tilt);
 	m->translate(tx,ty,tz);
-	//camera->rotate_y(1.5f);
+	camera->rotate_y(1.5f);
 	camera->zoom(0.5);
 	camera->update_transform();
 	
